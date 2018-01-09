74b7d7e4d79873c5857301ffa44227da6191da05
iamhenry <virtualhenry@gmail.com>
2018-01-08 17:04:42 -0800

    Added the search view to the storyboard

---
+++ itunesSearch/itunesSearch/SearchResultsController.swift
@@ -0,0 +1,38 @@
+//
+//  SearchResultsController.swift
+//  itunesSearch
+//
+//  Created by  Henry Moran on 1/8/18.
+//  Copyright © 2018 Henry Moran. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsController: UITableViewController {
+
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(SearchResultsController.dismissSearchResultsController))
+        
+        tableView.tableHeaderView = searchController.searchBar
+        searchController.dimsBackgroundDuringPresentation = false
+        searchController.searchResultsUpdater = self
+    }
+    
+    func dismissSearchResultsController() {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension SearchResultsController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        print(searchController.searchBar.text!)
+    }
+}
