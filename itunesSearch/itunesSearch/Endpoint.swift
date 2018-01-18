1d646b1eaadb3bab0bcec515d0fd5b45e7d03926
henry moran <virtualhenry@gmail.com>
2018-01-18 09:23:41 -0800

    Adding an enum for the URL endpoint

---
+++ itunesSearch/itunesSearch/Endpoint.swift
@@ -0,0 +1,13 @@
+//
+//  Endpoint.swift
+//  itunesSearch
+//
+//  Created by Henry Moran on 1/18/18.
+//  Copyright © 2018 Henry Moran. All rights reserved.
+//
+
+import Foundation
+
+enum Itunes {
+    case search(term: String, media: ItunesMedia?)
+}
